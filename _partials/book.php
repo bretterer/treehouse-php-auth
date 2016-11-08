8ee09dd0849af83ae78a094ed277db2c11e1ef81
Brian Retterer <brian@stormpath.com>
2016-11-08 14:04:40 -0500

    Add book item

---
+++ _partials/book.php
@@ -0,0 +1,8 @@
+<div class="media well">
+
+	<div class="media-body">
+		<h4 class="media-heading">BOOK NAME</h4>
+		<p>Bacon ipsum dolor amet short loin pork chop beef, sirloin tri-tip biltong pancetta turkey t-bone ham leberkas fatback. Burgdoggen sausage frankfurter cupim salami jowl fatback tongue. Beef ribs meatball burgdoggen pork chop. Kevin bacon pork ball tip capicola. </p>
+	</div>
+
+</div>
--- books.php
+++ books.php
@@ -9,15 +9,7 @@ require_once __DIR__ . '/_partials/nav.php';
 		<div class="well">
 			<h2>Book List</h2>
 
-			<div class="media well">
-
-
-				<div class="media-body">
-					<h4 class="media-heading">BOOK NAME</h4>
-					<p>Bacon ipsum dolor amet short loin pork chop beef, sirloin tri-tip biltong pancetta turkey t-bone ham leberkas fatback. Burgdoggen sausage frankfurter cupim salami jowl fatback tongue. Beef ribs meatball burgdoggen pork chop. Kevin bacon pork ball tip capicola. </p>
-				</div>
-
-			</div>
+			<?php include __DIR__ . '/_partials/book.php'; ?>
 
 		</div>
 	</div>
