cf0642bc8d8b12a9e843b1b63aa3bd922ca77fe2
Brian Retterer <brian@stormpath.com>
2016-11-08 13:36:08 -0500

    add books.php

---
+++ books.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/_partials/head.php';
+require_once __DIR__ . '/_partials/nav.php';
+?>
+
+
+
+	<div class="container">
+		<div class="well">
+			<h2>Book List</h2>
+
+			<div class="media well">
+
+
+				<div class="media-body">
+					<h4 class="media-heading">BOOK NAME</h4>
+					<p>Bacon ipsum dolor amet short loin pork chop beef, sirloin tri-tip biltong pancetta turkey t-bone ham leberkas fatback. Burgdoggen sausage frankfurter cupim salami jowl fatback tongue. Beef ribs meatball burgdoggen pork chop. Kevin bacon pork ball tip capicola. </p>
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+
+<?php
+require_once __DIR__ . '/_partials/footer.php';
